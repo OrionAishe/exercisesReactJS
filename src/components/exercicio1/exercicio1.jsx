ba6c61c8de84fb91b46ba365f9b2da469e7a2e3e
Orion Antonucci <wagantonucci@hotmail.com>
2022-06-13 10:36:58 -0300

    Do Exercise 1

---
+++ src/components/exercicio1/exercicio1.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Quadrado = (props) =>{
+    return <div className={props.class}>
+        <h1>Hello World</h1>
+    </div>
+}
+
+export default Quadrado;
--- src/components/exercicio1/index.jsx
+++ src/components/exercicio1/index.jsx
@@ -1,5 +1,10 @@
+import styles from "./index.module.scss";
+import Quadrado from "./exercicio1";
+
 function Exercicio1() {
-	return <h1 className='title'>Exercicio1</h1>
+	return <div className={styles.Exercicio1}><h1 className='title'>Exercicio1</h1>
+	<Quadrado class = {styles.Quadrado} />
+	</div>
 }
 
 export default Exercicio1
